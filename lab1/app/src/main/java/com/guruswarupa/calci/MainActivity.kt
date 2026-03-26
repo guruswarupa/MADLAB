08200f9bdf345bad242e47d0a38132b9726d9aac
Guru Swarupa <msgswarupa@gmail.com>
2026-03-26 11:14:26 +0530

    add calci android

--- lab1/app/src/main/java/com/guruswarupa/calci/MainActivity.kt
+++ lab1/app/src/main/java/com/guruswarupa/calci/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.guruswarupa.calci
+package com.guruswarupa.lab1
 
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.guruswarupa.calci.R
 
 class MainActivity : AppCompatActivity() {
 
